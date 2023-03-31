bbbe3dec0eed61d2faaa07198d1caa6035b3e447
Iyarodi <iyarodiinetimi@gmail.com>
2023-03-31 14:02:29 +0100

    Followed udemy tutorial up to this point.

---
+++ app.js
@@ -0,0 +1,72 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/4a9776be76";
+
+  const options = {
+    method: "POST",
+    auth: "iyarodi:f97c54cd897540ff378d0ba810e31b2a-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/sucess.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server is up and running on port 3000 bruv.");
+});
+
+// API key
+// f97c54cd897540ff378d0ba810e31b2a-us21
+
+// List id
+// 4a9776be76
